7ecd5a7a9533e952907465fd351718d455f79b14
TravisA <github email address>
2018-04-30 18:32:42 -0600

    create basic layout to test

--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {Grid} from 'react-bootstrap'
 import logo from './logo.svg';
+import NavbarContainer from './App/Navbar/NavbarContainer'
+import SectionContainer from './App/Section/SectionContainer'
 import './App.css';
+import './css/main.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div>
+          <NavbarContainer />
+          <Grid>
+            <SectionContainer id_string="home" />
+            <SectionContainer id_string="aboutUs" />
+            <SectionContainer id_string="embroidery" />
+            <SectionContainer id_string="screenPrinting" />
+          </Grid>
+          <h1>Test</h1>
+        </div>
+      </Router>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {}
+}
+
+export default connect(mapStateToProps, {})(App);
+
+// <NavBar />
+//           <Route exact path='/' component={}/>
+//           <Grid>
+//             <Route exact path='/' component={} />
+//             <Switch>
+//               <Route path={'/shelters/new'} component={} />
+//               <Route path={'/shelters/:shelterId'} component={} />
+//             </Switch>
+//           </Grid>
+          
+//           <Grid>
+//             {//SocialMediaLinks}
+//             }
+//           </Grid>
